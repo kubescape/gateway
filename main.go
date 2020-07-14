95577eb611cfdac03cf285548c53d85f5af0ae05
David Wertenteil <dwertent@cyberarmor.io>
2020-07-14 11:36:22 +0300

    rm tag

--- main.go
+++ main.go
@@ -2,30 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"notification-server/notificationserver"
 )
 
 func main() {
-	DisplayBuildTag()
+	// DisplayBuildTag()
 
 	ns := notificationserver.NewNotificationServer()
 	fmt.Printf("NewNotificationServer")
 	ns.SetupNotificationServer()
 }
 
-// DisplayBuildTag display on startup
-func DisplayBuildTag() {
-	imageVersion := "unknown build"
-	dat, err := ioutil.ReadFile("./build_number.txt")
-	if err == nil {
-		imageVersion = string(dat)
-	} else {
-		dat, err = ioutil.ReadFile("./build_date.txt")
-		if err == nil {
-			imageVersion = fmt.Sprintf("%s, date: %s", imageVersion, string(dat))
-		}
-	}
-	log.Printf("Image version: %s", imageVersion)
-}
+// // DisplayBuildTag display on startup
+// func DisplayBuildTag() {
+// 	imageVersion := "unknown build"
+// 	dat, err := ioutil.ReadFile("./build_number.txt")
+// 	if err == nil {
+// 		imageVersion = string(dat)
+// 	} else {
+// 		dat, err = ioutil.ReadFile("./build_date.txt")
+// 		if err == nil {
+// 			imageVersion = fmt.Sprintf("%s, date: %s", imageVersion, string(dat))
+// 		}
+// 	}
+// 	log.Printf("Image version: %s", imageVersion)
+// }
