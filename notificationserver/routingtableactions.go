3e6993c0fee89a5291fd681a16f46e57191416d2
david <dwertent@cyberarmor.io>
2021-02-17 15:11:03 +0200

    fix log

--- notificationserver/routingtableactions.go
+++ notificationserver/routingtableactions.go
@@ -52,7 +52,7 @@ func (cs *Connections) Remove(attributes map[string]string) {
 	slcLen := len(cs.connections)
 	for i := 0; i < slcLen; i++ {
 		if cs.connections[i].AttributesContained(attributes) {
-			glog.Infof("Removing connection from incoming list. index: %d. attributes: %v, id: %d, list len: %d", i, cs.connections[i].attributes, cs.connections[i].ID, len(cs.connections)-1)
+			glog.Infof("Removing connection from list. index: %d. attributes: %v, id: %d, list len: %d", i, cs.connections[i].attributes, cs.connections[i].ID, len(cs.connections)-1)
 			if slcLen == 1 { //i is the only element in the slice so we need to remove this entry from the map
 				cs.connections = []*Connection{}
 			} else if i == slcLen-1 { // i is the last element in the slice so i+1 is out of range
@@ -73,7 +73,7 @@ func (cs *Connections) RemoveID(id int) {
 	slcLen := len(cs.connections)
 	for i := 0; i < slcLen; i++ {
 		if cs.connections[i].ID == id {
-			glog.Infof("Removing connection from incoming list. index: %d. attributes: %v, id: %d, list len: %d", i, cs.connections[i].attributes, cs.connections[i].ID, len(cs.connections)-1)
+			glog.Infof("Removing connection from list. index: %d. attributes: %v, id: %d, list len: %d", i, cs.connections[i].attributes, cs.connections[i].ID, len(cs.connections)-1)
 			if slcLen == 1 { //i is the only element in the slice so we need to remove this entry from the map
 				cs.connections = []*Connection{}
 			} else if i == slcLen-1 { // i is the last element in the slice so i+1 is out of range
